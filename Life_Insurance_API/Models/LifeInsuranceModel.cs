d2d0a43e3282c3b7439c03940314bd01c3da36e1
Easibaja <easibaja06@gmail.com>
2025-04-13 07:59:36 -0600

    API functional. Swagger needs work.
    Added a token input on swagger for testing. API seems to work and returns expected values according to inputs. Return messages could use some work. Swagger lacking data explanation and format.

---
+++ Life_Insurance_API/Models/LifeInsuranceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LifeInsuranceApi.Models
+{
+    public class LifeInsuranceRequest
+    {
+        public DateTime DateOfBirth { get; set; }
+        public string Sex { get; set; } // "male" or "female"
+    }
+
+    public class LifeInsuranceResponse
+    {
+        public decimal MonthlyCost { get; set; }
+    }
+}
