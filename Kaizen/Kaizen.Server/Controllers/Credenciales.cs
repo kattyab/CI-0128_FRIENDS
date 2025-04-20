9c19caaa00400905b5460d8abba3ab762ee02fdc
IGNACIO ALEXANDER REDONDO ALFARO <ignacio.redondo@ucr.ac.cr>
2025-04-20 03:09:55 -0600

    Deploy swagger, we can make queries to the DB

---
+++ Kaizen/Kaizen.Server/Controllers/Credenciales.cs
@@ -0,0 +1,31 @@
+﻿using Kaizen.Server.Models;
+using Kaizen.Server.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaizen.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Credenciales : ControllerBase
+    {
+        private readonly Handlers.CredencialesH _employeeHandler;
+
+        // Constructor: ASP.NET Core inyecta el servicio EmpleadoHandler
+        public Credenciales(Handlers.CredencialesH employeeHandler)
+        {
+            _employeeHandler = employeeHandler;
+        }
+
+        [HttpGet("{email}")]
+        public IActionResult GetEmpleadoInfo(string email)
+        {
+            var empleado = _employeeHandler.ObtainUserData(email);
+            if (empleado == null)
+            {
+                return NotFound(new { message = "Empleado no encontrado." });
+            }
+
+            return Ok(empleado);
+        }
+    }
+}
