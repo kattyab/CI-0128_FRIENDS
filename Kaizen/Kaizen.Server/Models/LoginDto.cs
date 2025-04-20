ebd189e660bac6e6506205eb48af6e28ce689a77
IGNACIO ALEXANDER REDONDO ALFARO <ignacio.redondo@ucr.ac.cr>
2025-04-20 04:01:13 -0600

    Connect backend and frontend

---
+++ Kaizen/Kaizen.Server/Models/LoginDto.cs
@@ -0,0 +1,9 @@
+// Models/LoginDto.cs
+namespace Kaizen.Server.Models
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
