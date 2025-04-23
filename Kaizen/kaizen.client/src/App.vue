1fe3400f34bbcd0f74ed8e3acf6c25247d3dfa22
Andrik <andri@JEN-PC>
2025-04-22 19:01:31 -0600

    First version of the side menu added, styling and role pending.

--- Kaizen/kaizen.client/src/App.vue
+++ Kaizen/kaizen.client/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  /*import sideMenu from './components/sideMenu.vue';*/
 </script>
 
 <template>
