2492aef37f4cf65b1c6fa9c0e418f6d90a653e4a
Andrik <andri@JEN-PC>
2025-04-24 14:13:06 -0600

    Menu updated per role asigned

--- Kaizen/kaizen.client/src/App.vue
+++ Kaizen/kaizen.client/src/App.vue
@@ -2,12 +2,19 @@
   import Header from './components/Header.vue';
   import Footer from './components/Footer.vue';
   import employeeMenu from './components/employeeMenu.vue';
+  import employerMenu from './components/employerMenu.vue';
+  import { ref } from 'vue'
+
+  // Hard coded role for testing
+  localStorage.setItem('role', 'employer') // or 'employer'
+  const role = ref(localStorage.getItem('role') || 'employee')
 </script>
 
 <template>
   <Header v-if="$route.meta.hide_header !== true"></Header>
   <main class="app">
-    <employeeMenu />
+    <employeeMenu v-if="role === 'employee'" />
+    <employerMenu v-else-if="role === 'employer'" />
     <RouterView />
   </main>
   <Footer v-if="$route.meta.hide_footer !== true"></Footer>
