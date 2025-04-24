90a162a5f4755add3bb78d9623fb89e643ab2f55
Andrik <andri@JEN-PC>
2025-04-24 12:08:30 -0600

    Menu changes, added styling, memory and 2 menus

--- Kaizen/kaizen.client/src/App.vue
+++ Kaizen/kaizen.client/src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
   import Header from './components/Header.vue';
   import Footer from './components/Footer.vue';
-  /*import sideMenu from './components/sideMenu.vue';*/
+  import employeeMenu from './components/employeeMenu.vue';
 </script>
 
 <template>
   <Header v-if="$route.meta.hide_header !== true"></Header>
-  <main>
+  <main class="app">
+    <employeeMenu />
     <RouterView />
   </main>
   <Footer v-if="$route.meta.hide_footer !== true"></Footer>
