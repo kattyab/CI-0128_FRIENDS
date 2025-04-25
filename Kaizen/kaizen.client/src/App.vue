eaea477b5564848d5292b63437f71762310f0ec3
Andrik <andri@JEN-PC>
2025-04-24 18:00:28 -0600

    Names corrected and refactor of file

--- Kaizen/kaizen.client/src/App.vue
+++ Kaizen/kaizen.client/src/App.vue
@@ -1,20 +1,27 @@
 <script setup>
   import Header from './components/Header.vue';
   import Footer from './components/Footer.vue';
-  import employeeMenu from './components/employeeMenu.vue';
-  import employerMenu from './components/employerMenu.vue';
+  import empleadoMenu from './components/menus/empleadoMenu.vue';
+  import duenoMenu from './components/menus/duenoMenu.vue';
+  import supervisorMenu from './components/menus/supervisorMenu.vue';
+  import superAdminMenu from './components/menus/superAdminMenu.vue';
+  import adminMenu from './components/menus/adminMenu.vue';
   import { ref } from 'vue'
 
   // Hard coded role for testing
-  localStorage.setItem('role', 'employer') // or 'employer'
-  const role = ref(localStorage.getItem('role') || 'employee')
+  localStorage.setItem('role', 'Empleado') // or 'employer'
+  const role = ref(localStorage.getItem('role') || 'Empleado')
+
 </script>
 
 <template>
   <Header v-if="$route.meta.hide_header !== true"></Header>
   <main class="app">
-    <employeeMenu v-if="role === 'employee'" />
-    <employerMenu v-else-if="role === 'employer'" />
+    <empleadoMenu v-if="role === 'Empleado'" />
+    <duenoMenu v-else-if="role === 'Dueño'" />
+    <supervisorMenu v-else-if="role === 'Supervisor'" />
+    <superAdminMenu v-else-if="role === 'SuperAdmin'" />
+    <adminMenu v-else-if="role === 'Administrador'" />
     <RouterView />
   </main>
   <Footer v-if="$route.meta.hide_footer !== true"></Footer>
