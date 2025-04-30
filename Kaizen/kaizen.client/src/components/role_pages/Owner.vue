866dd370e7e86e6d8422de8fa9e35e35c46af108
Easibaja <easibaja06@gmail.com>
2025-04-29 19:12:51 -0600

    Added session by cookie, placeholder role pages, some router protection.

---
+++ Kaizen/kaizen.client/src/components/role_pages/Owner.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold text-blue-600">Dueño Page</h1>
+    <p>Bienvenido, Dueño. Aquí puedes gestionar tu empresa.</p>
+  </div>
+</template>
+
+<script setup></script>
