d6e560810734b6fb79b512844248f94efb0fb5bc
Katherine Acosta <katherine.acostabarquero@ucr.ac.cr>
2025-05-03 11:09:49 -0600

    Update file structure, store layouts in their own folder, move page files to the pages folder

--- Kaizen/kaizen.client/src/layouts/main.vue
+++ Kaizen/kaizen.client/src/layouts/main.vue
@@ -1,4 +1,4 @@
-<!-- layouts/MainLayout.vue -->
+<!-- layouts/main.vue -->
 <script setup>
 import Header         from '../components/Header.vue'
 import Footer         from '../components/Footer.vue'
