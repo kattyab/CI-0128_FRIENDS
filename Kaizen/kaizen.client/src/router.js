80aaa85bf610b091113e5075f273d7780e6f04b1
IGNACIO ALEXANDER REDONDO ALFARO <ignacio.redondo@ucr.ac.cr>
2025-05-06 21:40:40 -0600

    Fix supervisor menu

--- Kaizen/kaizen.client/src/router.js
+++ Kaizen/kaizen.client/src/router.js
@@ -10,14 +10,15 @@ const router = createRouter({
       children: [
         { path: '', name: 'Home', component: () => import('./pages/home.vue'), meta: { public: true } },
         { path: 'about', name: 'About', component: () => import('./pages/about.vue'), meta: { public: true } },
-        { path: 'landingpage', name: 'Landing-page', component: () => import('./pages/landing-page.vue'), meta: { requiresAuth: true } },
+        { path: 'landing-page', name: 'Landing-page', component: () => import('./pages/landing-page.vue'), meta: { requiresAuth: true } },
         { path: 'unauthorized', name: 'Unauthorized', component: () => import('./pages/unauthorized.vue'), meta: { public: true } },
         { path: 'registeremployee', name: 'RegisterEmployee', component: () => import('./pages/register-employee.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
         { path: 'companies', name: 'Companies Index', component: () => import('./pages/companies/index.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] } },
         { path: 'companies/:id', name: 'Companies Show', component: () => import('./pages/companies/show.vue'), meta: { requiresAuth: true } },
         { path: 'employees', name: 'Employees Index', component: () => import('./pages/employees/index.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] } },
         { path: 'employees/:id', name: 'EmployeeDetails', component: () => import('./pages/employees/show.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño', 'Superadmin'] } },
-        { path: 'companieslist', name: 'CompaniesList', component: () => import('./components/Companies_list.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] }},
+        { path: 'companieslist', name: 'CompaniesList', component: () => import('./components/Companies_list.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] } },
+        { path: 'review-hours', name: 'ReviewHours', component: () => import('./pages/review-hours.vue'), meta: { requiresAuth: true } },
       ]
     },
     {
@@ -27,6 +28,12 @@ const router = createRouter({
         { path: '', name: 'Login', component: () => import('./pages/login-user.vue') },
         { path: 'register-company', name: 'RegisterCompany', component: () => import('./pages/registerCompany.vue') },
       ],
+      meta: { public: true }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('./pages/NotFoundPage.vue'),
       meta: { public: true }
     }
   ]
