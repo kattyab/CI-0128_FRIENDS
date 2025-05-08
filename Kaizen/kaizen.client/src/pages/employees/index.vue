e644fc53a7a5f798154f80b3c0cfab29fff2900a
dannyelobaldia <164052574+dannyelobaldia@users.noreply.github.com>
2025-05-07 18:38:56 -0600

    Fixes language from english to spanish.

--- Kaizen/kaizen.client/src/pages/employees/index.vue
+++ Kaizen/kaizen.client/src/pages/employees/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <h1>Employees</h1>
+    <h1>Empleados</h1>
     <table class="table table-hover">
       <thead>
         <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Id</th>
-          <th scope="col">Job</th>
-          <th scope="col">Contract</th>
-          <th scope="col">Actions</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellidos</th>
+          <th scope="col">Cédula</th>
+          <th scope="col">Puesto de Trabajo</th>
+          <th scope="col">Tipo de Contrato</th>
+          <th scope="col">Acciones</th>
         </tr>
       </thead>
       <tbody class="table-group-divider">
