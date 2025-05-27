2a09882c2dee90bfb58a0354f04db47b19735109
Easibaja <easibaja06@gmail.com>
2025-05-27 14:42:49 -0600

    fixed API data with right names

--- Queries/12-InsertApiData.sql
+++ Queries/12-InsertApiData.sql
@@ -7,9 +7,9 @@ DECLARE @Benefit_VORLAG  UNIQUEIDENTIFIER = '40D9530F-5DC1-47ED-B3E6-34238C03F6F
 
 INSERT INTO dbo.Benefits (ID, Name, MinWorkDurationMonths, OfferedBy, IsAPI, Path, NumParameters, IsFixed, IsPercetange, IsFullTime, IsPartTime, IsByHours, IsByService)
 VALUES
-(@Benefit_FRIENDS, 'FRIENDS', 0, '8A74DCCE-3A51-4A58-AAB8-AD81B9432901', 1, 'https://poliza-friends-grg0h9g5crf2hwh8.southcentralus-01.azurewebsites.net/api/LifeInsurance', 2, 0, 0, 1, 1, 1, 1),
-(@Benefit_GEEMS,   'GEEMS',   0, '8A74DCCE-3A51-4A58-AAB8-AD81B9432901', 1, 'https://asociacion-geems-c3dfavfsapguhxbp.southcentralus-01.azurewebsites.net/api/public/calculator/calculate', 2, 0, 0, 1, 1, 1, 1),
-(@Benefit_VORLAG,  'Vorlagenersteller S.A.', 0, '8A74DCCE-3A51-4A58-AAB8-AD81B9432901', 1, 'https://mediseguro-vorlagenersteller-d4hmbvf7frg7aqan.southcentralus-01.azurewebsites.net/api/MediSeguroMonto', 3, 0, 0, 1, 1, 1, 1);
+(@Benefit_FRIENDS, 'Seguro de vida', 0, '8A74DCCE-3A51-4A58-AAB8-AD81B9432901', 1, 'https://poliza-friends-grg0h9g5crf2hwh8.southcentralus-01.azurewebsites.net/api/LifeInsurance', 2, 0, 0, 1, 1, 1, 1),
+(@Benefit_GEEMS,   'Asociación solidarista',   0, '8A74DCCE-3A51-4A58-AAB8-AD81B9432901', 1, 'https://asociacion-geems-c3dfavfsapguhxbp.southcentralus-01.azurewebsites.net/api/public/calculator/calculate', 2, 0, 0, 1, 1, 1, 1),
+(@Benefit_VORLAG,  'MediSeguro', 0, '8A74DCCE-3A51-4A58-AAB8-AD81B9432901', 1, 'https://mediseguro-vorlagenersteller-d4hmbvf7frg7aqan.southcentralus-01.azurewebsites.net/api/MediSeguroMonto', 3, 0, 0, 1, 1, 1, 1);
 
 INSERT INTO dbo.ApiDeductionConfigs (Name, Endpoint, HttpMethod, AuthHeaderName, AuthToken, ParametersJson, ExpectedDataType, BenefitsID)
 VALUES
