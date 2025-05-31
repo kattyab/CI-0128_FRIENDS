be58e6a166ef77343bbdc288432158ff73e44c53
dannyelobaldia <164052574+dannyelobaldia@users.noreply.github.com>
2025-05-30 18:51:19 -0600

    Fixed query to list subscribed APIs correctly

--- Kaizen/Kaizen.Server/Infrastructure/Repositories/Benefits/EmployeeBenefitListRepository.cs
+++ Kaizen/Kaizen.Server/Infrastructure/Repositories/Benefits/EmployeeBenefitListRepository.cs
@@ -25,26 +25,47 @@ namespace Kaizen.Server.Infrastructure.Repositories.Benefits
 
             var query = @"
                 SELECT 
-                    b.Name,
-                    CASE 
-                        WHEN b.IsFixed = 1 THEN 'Fixed'
-                        WHEN b.IsPercetange = 1 THEN 'Percentage'
-                        WHEN b.IsAPI = 1 THEN 'API'
-                        ELSE 'Other'
-                    END AS Type,
-                    CASE 
-                        WHEN b.IsFixed = 1 THEN CAST(b.FixedValue AS NVARCHAR(50))
-                        WHEN b.IsPercetange = 1 THEN CAST(b.PercentageValue AS NVARCHAR(50)) + '%'
-                        WHEN b.IsAPI = 1 THEN 'API Calculated'
-                        ELSE 'N/A'
-                    END AS Value,
-                    b.MinWorkDurationMonths AS MinMonths
-                FROM Users u
-                INNER JOIN Employees e ON u.PersonPK = e.PersonPK
-                INNER JOIN ChosenBenefits cb ON e.EmpID = cb.EmployeeID
-                INNER JOIN Benefits b ON cb.BenefitID = b.ID
-                WHERE u.Email = @Email
-                ORDER BY b.Name";
+                    combined.Name,
+                    combined.Type,
+                    combined.Value,
+                    combined.MinMonths
+                FROM (
+                    -- Original query for Benefits
+                    SELECT 
+                        b.Name,
+                        CASE 
+                            WHEN b.IsFixed = 1 THEN 'Fixed'
+                            WHEN b.IsPercetange = 1 THEN 'Percentage'
+                            ELSE 'Other'
+                        END AS Type,
+                        CASE 
+                            WHEN b.IsFixed = 1 THEN CAST(b.FixedValue AS NVARCHAR(50))
+                            WHEN b.IsPercetange = 1 THEN CAST(b.PercentageValue AS NVARCHAR(50)) + '%'
+                            ELSE 'N/A'
+                        END AS Value,
+                        b.MinWorkDurationMonths AS MinMonths,
+                        0 AS IsApi
+                    FROM Users u
+                    INNER JOIN Employees e ON u.PersonPK = e.PersonPK
+                    INNER JOIN ChosenBenefits cb ON e.EmpID = cb.EmployeeID
+                    INNER JOIN Benefits b ON cb.BenefitID = b.ID
+                    WHERE u.Email = @Email
+    
+                    UNION ALL
+    
+                    SELECT 
+                        adc.Name,
+                        'IsApi' AS Type,
+                        'API' AS Value,
+                        0 AS MinMonths,
+                        1 AS IsApi -- Flag to identify source
+                    FROM Users u
+                    INNER JOIN Employees e ON u.PersonPK = e.PersonPK
+                    INNER JOIN ChosenAPIs capi ON e.EmpID = capi.EmployeePK
+                    INNER JOIN ApiDeductionConfigs adc ON capi.ApiID = adc.Id
+                    WHERE u.Email = @Email
+                ) AS combined
+                ORDER BY combined.Name;";
 
             using var command = new SqlCommand(query, connection);
             command.Parameters.AddWithValue("@Email", email);
