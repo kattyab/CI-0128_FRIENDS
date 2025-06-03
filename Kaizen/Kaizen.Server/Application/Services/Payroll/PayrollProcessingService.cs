23e2535dec6093df0d9d1565ddd801c09f137ccb
Easibaja <easibaja06@gmail.com>
2025-06-03 08:58:54 -0600

    Added employee filtering if they register hours

--- Kaizen/Kaizen.Server/Application/Services/Payroll/PayrollProcessingService.cs
+++ Kaizen/Kaizen.Server/Application/Services/Payroll/PayrollProcessingService.cs
@@ -112,9 +112,11 @@ namespace Kaizen.Server.Application.Services.Payroll
                     dbo.GetPayrollTypeDescription(C.PayrollType) AS PayrollTypeDescription
                 FROM dbo.Employees E
                 INNER JOIN dbo.Companies C ON E.WorksFor = C.CompanyPK
+                LEFT JOIN dbo.ApprovedHours AH ON E.EmpID = AH.EmpID AND AH.Status = 'Approved'
                 WHERE 
                     C.CompanyPK = @CompanyID
-                AND (E.IsDeleted = 0 OR E.IsDeleted IS NULL);";
+                AND (E.IsDeleted = 0 OR E.IsDeleted IS NULL)
+                AND (E.RegistersHours = 0 OR (E.RegistersHours = 1 AND AH.EmpID IS NOT NULL));";
 
             await using var command = new SqlCommand(cmdText, connection);
             command.Parameters.AddWithValue("@CompanyID", companyId.ToString());
