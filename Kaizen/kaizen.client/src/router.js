89e95396c6e2d53c4f63c27a3adf42b84008078b
dannyelobaldia <164052574+dannyelobaldia@users.noreply.github.com>
2025-06-30 00:36:10 -0600

    Create isCompanyDeleted metadata to throw 403 error page when company is soft deleted.

--- Kaizen/kaizen.client/src/router.js
+++ Kaizen/kaizen.client/src/router.js
@@ -12,25 +12,25 @@ const router = createRouter({
         { path: 'about', name: 'About', component: () => import('./pages/about.vue'), meta: { public: true } },
         { path: 'landing-page', name: 'Landing-page', component: () => import('./pages/landing-page.vue'), meta: { requiresAuth: true } },
         { path: 'unauthorized', name: 'Unauthorized', component: () => import('./pages/errors/403.vue'), meta: { public: true } },
-        { path: 'company/edit', name: 'Company Edit', component: () => import('./pages/company/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Dueño'] } },
+        { path: 'company/edit', name: 'Company Edit', component: () => import('./pages/company/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Dueño'], requiresCompanyActive: true } },
         { path: 'companies', name: 'Companies Index', component: () => import('./pages/companies/index.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] } },
         { path: 'companies/:id', name: 'Show Company', component: () => import('./pages/companies/show.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño', 'Superadmin'] } },
-        { path: 'companies/:id/edit', name: 'Edit Company', component: () => import('./pages/companies/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] } },
+        { path: 'companies/:id/edit', name: 'Edit Company', component: () => import('./pages/companies/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'], requiresCompanyActive: true } },
         { path: 'companieslist', name: 'CompaniesList', component: () => import('./pages/companies/companiesList.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] } },
         { path: 'companieslist/:id', name: 'Companies Show', component: () => import('./pages/companies/show.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Superadmin'] } },
         { path: 'companyemployees', name: 'Companies Employees', component: () => import('./pages/employees/ByCompanyIndex.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
         { path: 'employees', name: 'Employees Index', component: () => import('./pages/employees/index.vue'), meta: { requiresAuth: true, requiredRoles: ['Superadmin'] } },
-        { path: 'employees/register', name: 'Register Employee', component: () => import('./pages/employees/register.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
+        { path: 'employees/register', name: 'Register Employee', component: () => import('./pages/employees/register.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'], requiresCompanyActive: true } },
         { path: 'employees/:id', name: 'Employee Details', component: () => import('./pages/employees/show.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño', 'Superadmin'] } },
-        { path: 'employees/:id/edit', name: 'Employee Edit', component: () => import('./pages/employees/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño', 'Superadmin'] } },
+        { path: 'employees/:id/edit', name: 'Employee Edit', component: () => import('./pages/employees/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño', 'Superadmin'], requiresCompanyActive: true } },
         { path: 'review-hours', name: 'ReviewHours', component: () => import('./pages/review-hours.vue'), meta: { requiresAuth: true, requiredRoles: ['Supervisor'] } },
         { path: 'benefits', name: 'Benefits List', component: () => import('./pages/benefits/index.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
-        { path: 'benefits/create', name: 'Create Benefit', component: () => import('./pages/benefits/create.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
+        { path: 'benefits/create', name: 'Create Benefit', component: () => import('./pages/benefits/create.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'], requiresCompanyActive: true } },
         { path: 'benefits/:id', name: 'Show Benefit', component: () => import('./pages/benefits/show.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
-        { path: 'benefits/:id/edit', name: 'Edit Benefit', component: () => import('./pages/benefits/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
+        { path: 'benefits/:id/edit', name: 'Edit Benefit', component: () => import('./pages/benefits/edit.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'], requiresCompanyActive: true } },
         { path: 'benefits/subscribe', name: 'Benefit Subscription', component: () => import('./pages/benefits/subscribe.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Supervisor', 'Empleado'] } },
         { path: 'company', name: 'Company', component: () => import('./pages/companies/company.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño'] } },
-        { path: 'registerhours', name: 'Register Hours', component: () => import('./pages/employees/registerHours.vue'), meta: { requiresAuth: true, requiredRoles: ['Empleado'], requiresRegistersHours: true } },
+        { path: 'registerhours', name: 'Register Hours', component: () => import('./pages/employees/registerHours.vue'), meta: { requiresAuth: true, requiredRoles: ['Empleado'], requiresRegistersHours: true, requiresCompanyActive: true } },
         { path: 'payroll',name: 'Payroll',component: () => import('./pages/payroll/payroll.vue'), meta: { requiresAuth: true, requiredRoles: ['Administrador', 'Dueño', ] },
         },
       ]
@@ -58,6 +58,7 @@ router.beforeEach(async (to, from, next) => {
   const requiresAuth = to.meta.requiresAuth;
   const requiredRoles = to.meta.requiredRoles;
   const requiresRegistersHours = to.meta.requiresRegistersHours;
+  const requiresCompanyActive = to.meta.requiresCompanyActive;
 
   if (isPublic || !requiresAuth) return next();
 
@@ -77,6 +78,16 @@ router.beforeEach(async (to, from, next) => {
         return next('/unauthorized');
       }
     }
+
+    if (requiresCompanyActive) {
+      const isCompanyDeleted = await axios.get(`${import.meta.env.VITE_API_URL}/api/Auth/isCompanyDeleted`, { withCredentials: true });
+
+      if (isCompanyDeleted) {
+        return next('/unauthorized');
+      }
+    }
+
+
 
     next();
   } catch (err) {
