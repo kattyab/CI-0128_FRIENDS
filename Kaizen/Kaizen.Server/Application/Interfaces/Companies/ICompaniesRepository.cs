5ce4fadd89c43b149b25c7a743143838177f97e6
dannyelobaldia <164052574+dannyelobaldia@users.noreply.github.com>
2025-07-01 23:36:18 -0600

    Changes on conflicting namespace

--- Kaizen/Kaizen.Server/Application/Interfaces/Companies/ICompaniesRepository.cs
+++ Kaizen/Kaizen.Server/Application/Interfaces/Companies/ICompaniesRepository.cs
@@ -2,14 +2,15 @@
 using Kaizen.Server.Application.Dtos.Companies;
 using Kaizen.Server.Infrastructure.Contexts;
 
-namespace Kaizen.Server.Infrastructure.Repositories;
-
-public interface ICompaniesRepository
+namespace Kaizen.Server.Application.Interfaces.Companies
 {
-    List<CompanyDto> GetCompanies();
-    CompanyDto? GetCompany(Guid companyPK);
-    void UpdateCompany(Guid companyPK, CompanyEditDto companyEditDto);
-    Task<bool> IsTherePayrollAsync(Guid companyPK, TransactionContext context);
-    Task SoftDeleteCompanyAsync(Guid companyPK, TransactionContext context);
-    Task FullDeleteCompanyAsync(Guid companyPK, TransactionContext context);
+    public interface ICompaniesRepository
+    {
+        List<CompanyDto> GetCompanies();
+        CompanyDto? GetCompany(Guid companyPK);
+        void UpdateCompany(Guid companyPK, CompanyEditDto companyEditDto);
+        Task<bool> IsTherePayrollAsync(Guid companyPK, TransactionContext context);
+        Task SoftDeleteCompanyAsync(Guid companyPK, TransactionContext context);
+        Task FullDeleteCompanyAsync(Guid companyPK, TransactionContext context);
+    }
 }
