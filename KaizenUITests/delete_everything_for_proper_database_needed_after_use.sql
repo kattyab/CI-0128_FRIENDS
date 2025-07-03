feea4de1be4eb87d91b7b0e48aace643606a0087
Easibaja <easibaja06@gmail.com>
2025-07-03 14:49:53 -0600

    scripts required to run it properly if your db is not properly configured

---
+++ KaizenUITests/delete_everything_for_proper_database_needed_after_use.sql
@@ -0,0 +1,64 @@
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+EXEC sp_MSforeachtable '
+IF ''?'' NOT LIKE ''%ApiDeductionConfigs%''
+BEGIN
+    PRINT ''Deleting from ?''
+    DELETE FROM ?
+END'
+
+Select * from ApiDeductionConfigs
